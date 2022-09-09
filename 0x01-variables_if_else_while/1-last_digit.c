f1c38efdb2a747646db875bc6740662386e68f1d
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-09 22:16:28 +0100

    ghn

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /** 
- * main - print if the number is positive, zero, or negative
- * Description: using the main function
- * this program prints "Programming is postive, zero, or negative"
- * Return: 0
- */
+* main - print if the number is positive, zero, or negative
+* Description: using the main function
+* this program prints "Programming is postive, zero, or negative"
+* Return: 0
+*/
 int main(void)
 {
 		int n;
