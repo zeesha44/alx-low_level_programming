43da569bd56d50242e6c67e5412d945766bf51da
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-09 23:16:03 +0100

    cvbn

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,11 +5,11 @@
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative"
  * Return: 0
- */ 
-int main (void)
+ */
+int main(void)
 {
 	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++) 
 	{
 		putchar(ch);
 	}
