c0f6601b384f3fbec9af11b7a0308d8499f1c09b
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-11 23:17:09 +0100

    fghj

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar(' ');
 			putchar((d2 / 10) + '0');
 			putchar((d2 % 10) + '0');
-			if (d1 == 98 && d2 == 99)
+			if (d1 == 97 && d2 == 98)
 				continue;
 			putchar(',');
 			putchar(' ');
