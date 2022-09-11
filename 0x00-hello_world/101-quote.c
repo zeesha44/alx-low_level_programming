118eb1eb6fcc6ee25ee4d1801f9b4eacde307f8c
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-11 22:19:13 +0100

    gdfd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,10 @@
 #include <unistd.h>
 /**
- * main - progra "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- * return (1);
+ * main - program entry point
+ * Return: 1 on success
+ */
+ int main(void)
+ {
+ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ return (1);
+ }
