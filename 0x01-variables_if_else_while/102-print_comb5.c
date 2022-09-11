6bd0bdcf6c9a2aeadab9d1951ba8774b1b02641a
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-11 23:11:04 +0100

    dgfhh

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,29 @@
 #include <stdio.h>
 /**
+ * main - prints all possible combinations of two different digits
  *
- *  * main - prints all possible combinations of two different digits
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
-
+ * Return: always 0
+ */
 int main(void)
-
 {
+	int d1, d2;
 
-		int d1, d2;
+	for (d1 = 0 ; d1 < 98 ; d1++)
+	{
+		for (d2 = d1 + 1 ; d2 < 99 ; d2++)
+		{
+			putchar((d1 / 10) + '0');
+			putchar((d1 % 10) + '0');
+			putchar(' ');
+			putchar((d2 / 10) + '0');
+			putchar((d2 % 10) + '0');
+			if (d1 == 98 && d2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 
-
-
-			for (d1 = 0 ; d1 < 9 ; d1++)
-
-					{
-
-								for (d2 = d1 + 1 ; d2 < 10 ; d2++)
-
-											{
-
-															putchar((d1 % 10) + '0');
-
-																		putchar((d2 % 10) + '0');
-
-																					if (d1 == 8 && d2 == 9)
-
-																										continue;
-
-																								putchar(',');
-
-																											putchar(' ');
-
-
-
-																													}
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+		}
+	}
+	putchar('\n');
+	return (0);
 }
