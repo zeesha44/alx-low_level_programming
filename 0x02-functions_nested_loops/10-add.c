6088ac75c8388c4161decd2d87d0b00affb83c51
zeesha44 <shaheeda.nazif@gmail.com>
2022-09-13 15:48:16 -0700

    redcx

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,10 @@
 /**
  * add - Adds two integers.
  * @num1: The first integer to be added.
- *
  * @num2: The second integer to be added.
  * Return : answer
  */
 int add(int num1, int num2)
 {
 	return (num1 + num2);
-}	
+}
