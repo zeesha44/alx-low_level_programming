8ea6baa26e64b380bcb6ecbf7ace22eb0e5262ac
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-15 23:32:08 +0100

    vcbv

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * print_line - prints a line
- *
- * @n:integer to print stuff
- */
+*print_line - prints a straight line
+*@n: parameter
+*Return:returns nothing
+*/
+
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	while (n-- > 0)
 	{
-		putchar(");
+		putchar('_');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
