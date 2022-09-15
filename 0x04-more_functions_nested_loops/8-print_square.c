19088eb7707b56cdc539490d4af6df87df622307
zeesha44 <shaheeda.nazif@gmail.com>
2022-09-15 15:50:01 -0700

    fdc

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,11 +2,10 @@
 #include "main.h"
 
 /**
-*print_square - prints squares
-*@size: parameter
-*Return: returns nothing
-*/
-
+ * print_square - prints squares
+ * @size: parameter
+ * Return: returns nothing
+ */
 void print_square(int size)
 {
 	int inc1, inc2;
@@ -15,11 +14,10 @@ void print_square(int size)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			for (inc2 = 0; inc2 < (size -1); inc2++)
 			{
 				putchar('#');
 			}
-
 			putchar('#');
 			putchar('\n');
 		}
@@ -29,3 +27,4 @@ void print_square(int size)
 		putchar('\n');
 	}
 }
+
