05a527f0198ea8891a1b77da70723d857510e5ae
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-19 21:57:52 +0100

    trfd

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
+#include "main.h"
 #include <stdio.h>
-void reset_to_98(int *n)
-	{
-	*n = 98;
-	}
-
 /**
  * main - check the code
  *
  * Return: always 0
  */
-int main(void)
+void reset_to_98(int *n)
 {
 	int n;
 
