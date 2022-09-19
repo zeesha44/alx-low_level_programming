e06152bdd435e211c7511b0853855dea3e1fdd2f
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-19 22:45:30 +0100

    xcvb

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * rev_string - Reverses a string
 * @s: string to be reversed
 *
 * Return: nothing
 */
-
 void rev_string(char *s)
 {
 	int i, tmp, len = _strlen(s);
