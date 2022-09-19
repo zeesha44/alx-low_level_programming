352aa04e5882bd0977f659b9046e3df2ac03a51f
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-19 21:59:54 +0100

    wesdrfg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,13 +5,10 @@
  *
  * Return: always 0
  */
-void reset_to_98(int *a)
+void reset_to_98(int *n)
 {
-	int n;
-
 	n = 402;
 	printf("n=%d\n", n);
 	reset_to_98(&n);
 	printf("n=%d\n", n);
-	return (0);
 }
