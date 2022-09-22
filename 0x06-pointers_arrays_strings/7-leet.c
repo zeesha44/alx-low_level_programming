281419e6c3246cd6b67eb03f04f770aaa55dcf26
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-09-22 21:36:47 +0100

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,23 @@
 #include "main.h"
-/**
- * leet - leet
- * @s: string
- *
- * Return: char value
- */
 
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
 char *leet(char *s)
 {
-	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
-	int i;
-	int j;
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; alphaArr[j] != '\0'; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (s[i] == alphaArr[j])
-			{
-				s[i] = alphaArra[j +1];
-				break;
-			}
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
 	}
 	return (s);
