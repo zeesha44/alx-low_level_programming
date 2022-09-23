50a0a616318b2341aa71ab018ed4ff5ea578e426
zeesha44 <shaheeda.nazif@gmail.com>
2022-09-23 10:10:43 -0700

    redszx

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _strncat - concatenate two strings
- * @dest: string to be appended to  @src: string to be concatenated at the end of @dest
+ * @dest: string to be appended to 
+ * @src: string to be concatenated at the end of @dest
  * @n: integer parameter to compare index
  *
  * Return: the concatenated string
@@ -15,5 +16,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
+	
 	return (dest);
 }
