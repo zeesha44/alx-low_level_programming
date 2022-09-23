e91d5f9c0fd4bf421dafc711f930025c94d07a7a
zeesha44 <shaheeda.nazif@gmail.com>
2022-09-23 10:25:10 -0700

    redcx

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,34 +7,14 @@
  *
  * Return: value of string
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff = 0;
+	int i = 0;
 
-	while (1)
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-		{
-			break;
-		}
-		else if (s1[i] == '\0')
-		{
-			diff = s2[i];
-			break;
-		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s1[i] != s2[i])
-		{
-			diff = s1[i] - s2[i];
-			break;
-		}
-		else{
-			i++;
-		}
+		i++;
 	}
-	return (diff);
+	return (s1[i] - s2[i]);
 }
