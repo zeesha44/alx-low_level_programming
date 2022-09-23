1937c1192f35f68d7af3d908523863080c34e4a8
zeesha44 <shaheeda.nazif@gmail.com>
2022-09-23 10:21:23 -0700

    rfdc

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,10 @@
 
 /**
  * _strncpy - copies a string
- * @dest: buffer storing the string copy
- * @src: source string
- * @n: maximum number of byte copied
- *
- * Return: dest
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -14,10 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	       dest[i] = src[i];
-	
-	for ( ; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
 		dest[i] = '\0';
-	
+	}
 	return (dest);
-}	
+}
