4964b2ace73be84018b650731876725d5e21351b
zeesha44 <shaheeda.nazif@gmail.com>
2022-10-11 03:52:48 -0700

    tgrfds

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#ifndef STRTYP
-#define STRTYP
+#define DOG_PROFILE
+#define DOG_PROFILE
 
 /**
- * struct dog - structure of a dog
+ * struct dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -14,12 +14,3 @@ struct dog
 	float age;
 	char *owner;
 };
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /* STRTYP */
