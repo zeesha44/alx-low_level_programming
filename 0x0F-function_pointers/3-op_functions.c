aa7f2d54aadda8dcfb51557b6644879ce8923217
zeesha44 <shaheeda.nazif@gmail.com>
2022-10-13 14:03:08 -0700

    rfvdczx

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,10 +41,12 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b != 0)
+	if (b == 0)
 	{
-	return (a / b);
+		printf("Error\n");
+		exit(100);
 	}
+	return (a / b);
 }
 /**
  * op_mod - returns the remainder of division
