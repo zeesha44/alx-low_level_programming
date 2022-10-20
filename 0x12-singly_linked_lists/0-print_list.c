773ab913fdf16d4dba43684455e0dd9f3f2f9636
zeesha44 <shaheeda.nazif@gmail.com>
2022-10-20 14:34:35 -0700

    trfdc

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,9 @@
+#include "list.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: singly linked list
+ * Return: number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n
