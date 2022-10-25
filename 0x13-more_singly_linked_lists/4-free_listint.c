57fa462fe7fc7d9b941dd07e12bc7f268b4813f2
zeesha44 <shaheeda.nazif@gmail.com>
2022-10-25 15:44:27 -0700

    rfdv

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of list
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
